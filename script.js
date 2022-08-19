b7c48cc3eea958598addc4645e329752535953bf
rajesh <krajeshgowri7032@mail.com>
2022-08-19 18:05:35 +0530

    Javascript-Events

---
+++ script.js
@@ -0,0 +1,24 @@
+function clickevent()
+{
+    document.write("Any One Can Code");
+}
+
+
+function mouseDown(){
+    document.getElementById("mouse").style.color="pink";
+}
+
+
+function mouseUp(){
+    document.getElementById("mouse").style.color="red";
+}
+
+
+function mouseOver(){
+    document.getElementById("over").innerHTML="😂, 😃, 🧘🏻‍♂️, 🌍, 🌦️, 🍞, 🚗, 📞, 🎉, ❤️, 🍆, 🏁,";
+}
+
+
+function myFunction() {
+    document.getElementById("ondblclick").innerHTML="Have A Nice Day";
+}
